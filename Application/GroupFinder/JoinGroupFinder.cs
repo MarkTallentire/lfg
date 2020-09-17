22c856e507437b15bbcc67ae57a5c87318d067f1
Mark Tallentire <olemus@live.co.uk>
2020-09-17 22:22:57 +0100

    Removed lowercase text from frontend

---
+++ Application/GroupFinder/JoinGroupFinder.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Application.Interfaces.AuthenticatedUser;
+using Data;
+using MediatR;
+
+namespace Application
+{
+    public class GroupFinder
+    {
+        public class Request : IRequest<Unit>
+        {
+            
+        }
+
+
+        public class RequestHandler : IRequestHandler<Request, Unit>
+        {
+            private readonly DataContext _context;
+            private readonly IAuthenticatedUserService _authenticatedUserService;
+
+            public RequestHandler(DataContext context, IAuthenticatedUserService authenticatedUserService)
+            {
+                _context = context;
+                _authenticatedUserService = authenticatedUserService;
+            }
+            
+            public Task<Unit> Handle(Request request, CancellationToken cancellationToken)
+            { 
+                
+            }
+        }
+    }
+}
---
+++ Data/Migrations/20200917101538_AddGroupFinder.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Data.Migrations
+{
+    public partial class AddGroupFinder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "GroupFinderId",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "GroupFinder",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(nullable: true),
+                    QueueType = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupFinder", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_GroupFinderId",
+                table: "AspNetUsers",
+                column: "GroupFinderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_GroupFinder_GroupFinderId",
+                table: "AspNetUsers",
+                column: "GroupFinderId",
+                principalTable: "GroupFinder",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_GroupFinder_GroupFinderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropTable(
+                name: "GroupFinder");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_GroupFinderId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "GroupFinderId",
+                table: "AspNetUsers");
+        }
+    }
+}
